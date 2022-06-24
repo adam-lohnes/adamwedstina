c34bcab1e44a82d3e675d60bfc26ab5cbffc5325
Adam <adam.a.lohnes@gmail.com>
2022-06-24 01:09:35 -0700

    add structure & functionality

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('boop!');
+let programHeader = document.querySelector('.program-header');
+let menuHeader = document.querySelector('.menu-header');
+
+let programList = document.querySelector('.program-list');
+let menuList = document.querySelector('.menu-list');
+
+programHeader.addEventListener('click', () => {
+    programHeader.classList.add('active');
+    menuHeader.classList.remove('active');
+    programList.classList.add('active');
+    menuList.classList.remove('active');
+});
+
+menuHeader.addEventListener('click', () => {
+    programHeader.classList.remove('active');
+    menuHeader.classList.add('active');
+    programList.classList.remove('active');
+    menuList.classList.add('active');
+});
